099af669925917a33d35002547f9742ff000040b
Vincent Anderson <vincentanderson@vincents-mbp.home>
2020-02-16 17:58:43 -0800

    Node variables created and set in components

--- Pac-Man-2020/Assets/Scripts/Node.cs
+++ Pac-Man-2020/Assets/Scripts/Node.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
+    public Node[] neighbors;
+    public Vector2[] validDir;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
