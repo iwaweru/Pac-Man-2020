7184603ae74b5549dd82cdc920008b5cd04e5c5e
Thomas Alexandre Kocer <thomasalexandrekocer@Thomass-MBP-2.home>
2020-02-29 09:52:35 -0800

    fixing missing ghost file

---
+++ Pac-Man-2020/Assets/Scripts/GhostController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : ControllerNodes
+{
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        this.canReverse = false;//Ghosts cannot move unless they are at an intersection.
+
+        transform.position = new Vector2(7, 10);//Ghost must start at node for now.
+
+        Node current = getNodeAtPosition(transform.position);//Get node at this position.
+        if (current != null)
+        {
+            currentNode = current;
+            Debug.Log(currentNode);
+        }
+
+        direction = Vector2.left;//Auto start.
+        ChangePosition(direction);
+    }
+
+    public override void Update() //Override to change behavior
+    {
+
+        randomInput();
+
+        Move();
+
+    }
+
+}
