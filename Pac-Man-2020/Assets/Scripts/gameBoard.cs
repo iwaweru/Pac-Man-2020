99678f99a6447740c4d0a8c0ebc6a05d61bc4176
Xnetter <ursingarrett@gmail.com>
2020-03-04 22:19:12 -0500

    Refactored GameBoard Script.

--- Pac-Man-2020/Assets/Scripts/gameBoard.cs
+++ Pac-Man-2020/Assets/Scripts/gameBoard.cs
@@ -21,7 +21,6 @@ public class gameBoard : MonoBehaviour
     public static int DEATH_DELAY = 5;
 
     //Array of type GameObject initialized with board width and height
-    //These are the locations that will be stored
     //We are getting the positions of the game objects and then storing them at that position in this array.
     public GameObject[,] board = new GameObject[boardWidth, boardHeight];
 
@@ -31,14 +30,12 @@ public class gameBoard : MonoBehaviour
         //Create an array of objects containing every objects in the scene
         Object[] objects = GameObject.FindObjectsOfType(typeof(GameObject));
 
-        //Then iterate over that array
-        //Assign each object to the variable "o"
         foreach (GameObject o in objects)
 		{
             //Get the positions: 
             Vector2 pos = o.transform.position; // we use "position" (instead of "localposition") which is in the global space of Unity. 
 
-            //Sanity check: we only want to store the objects in the array (pills, walls, etc.) not PacMan itself. 
+            // we only want to store the objects in the array (pills, walls, etc.) not PacMan itself. 
             if (o.name != "Clyde" && o.name != "Pac-Man-Node" && o.name != "Game" && o.name != "Maze" && o.name != "Pills" && o.name != "Nodes" && o.name != "Background" &&  o.name != "NonNodes" && o.name != "Overlay" && o.name != "Blinky" && o.name != "Inky")
 			{
                 //if (o.GetComponent<Pills>() != null) {
@@ -46,7 +43,6 @@ public class gameBoard : MonoBehaviour
                 //        totalPellets++;
                 //    }
                 //}
-                //store the object o in the board array
                 Debug.Log("X: " + (int)pos.x + " Y: " + (int)pos.y + " " + o.name);
                 board[(int)pos.x, (int)pos.y] = o;
                 //Debug.Log(board[(int)pos.x, (int)pos.y]);
@@ -77,7 +73,7 @@ public class gameBoard : MonoBehaviour
     {
         if(time != 0)
         {
-            time -= (float)(1.0/24.0);
+            time -= (float)(1.0/24.0);//Every frame we substract one frame second from time bank. When bank hits zero, stop the munching sound.
             if(time < 0)
             {
                 time = 0;
