2202bb5dec366618042e39bfa0d115593134294d
Vincent Anderson <vincentanderson@vincents-mbp.home>
2020-05-01 13:40:42 -0700

    Menu play button now turns red when enter held, gameOver sprite added as well.

---
+++ Pac-Man-2020/Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    SpriteRenderer playButton;
+   
+    void Start()
+    {
+        playButton = GetComponent<SpriteRenderer>();
+    }
+    
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Return))
+        {            
+            // Change the 'color' property of the 'Sprite Renderer'
+            playButton.color = new Color (1, 0, 0, 1);
+        } else {
+            playButton.color = new Color (1, 1, 1, 1);
+        }
+    }
+}
